e2e92faee0b3a64061075026a47dd477ffe01178
Philip Dourachalis <82515894+filipposdch@users.noreply.github.com>
2021-11-21 14:24:03 +0200

    upload example

---
+++ minipythonexample.py
@@ -0,0 +1,8 @@
+#! /usr/bin/env python
+#A miniPython example       
+import tensorflow as tf
+from tensorflow.python.keras.layers import Input, Dense
+
+def student(firstname, lastname ='Mark', standard ='Fifth'):
+     a = a**2 -2*a*b + b**2
+     print firstname, lastname, 'studies in', standard, 'Standard'
