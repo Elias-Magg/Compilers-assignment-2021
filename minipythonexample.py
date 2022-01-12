81b9eddf24d200d4312a44593cc439658fefd3c6
Philip Dourachalis <82515894+filipposdch@users.noreply.github.com>
2022-01-12 13:47:59 +0200

    update with appropriate error examples

--- minipythonexample.py
+++ minipythonexample.py
@@ -6,3 +6,13 @@ from tensorflow.python.keras.layers import Input, Dense
 def student(firstname, lastname ='Mark', standard ='Fifth'):
      a = a**2 -2*a*b + b**2
      print firstname, lastname, 'studies in', standard, 'Standard'
+     
+def y(a, b, c): //removing ',' shows "missing )"
+	y = 5
+	a = [1,2,3,4,5]
+	a=[b] //removing ']' shows "missing ], +, -, or ,
+	if a < -5 or a >= b:
+		a = 5
+def x():
+	k =10
+ print "A", 6, undef //removing , shows appropriate missing error
