81caab2c1fb70942138b2dd05fa070a0ee7d7f09
Philip <filippos.dourachalis@gmail.com>
2022-01-16 23:02:02 +0200

    add operation typechecks

--- src/SecondVisitor.java
+++ src/SecondVisitor.java
@@ -1,8 +1,5 @@
 import minipython.analysis.DepthFirstAdapter;
-import minipython.node.AArraycellStatement;
-import minipython.node.AAssignStatement;
-import minipython.node.AFunctionCall;
-import minipython.node.PValue;
+import minipython.node.*;
 
 import java.util.ArrayList;
 import java.util.Hashtable;
@@ -21,6 +18,8 @@ public class SecondVisitor extends DepthFirstAdapter {
     private Hashtable<String, FunCall> fcalls;
 
     private FunCall currentFunc; //keep the most recently found function definition
+    private int currentLine;
+    private boolean returning;
 
     public SecondVisitor(Hashtable<String, List<FunctionArgs>> functions, Hashtable<String, Type> vars, Hashtable<String, FunCall> fcalls) {
         this.functions = functions;
@@ -30,34 +29,134 @@ public class SecondVisitor extends DepthFirstAdapter {
 
     //Assignment statements
     @Override
-    public void inAAssignStatement(AAssignStatement node)
-    {
+    public void inAAssignStatement(AAssignStatement node) {
         String varName = Utils.extractId(node.getIdentifier()).toString();
         Type type = Utils.extractType(node.getExpression(), functions, vars, currentFunc);
         vars.put(varName, type);
     }
 
     @Override
-    public void inAArraycellStatement(AArraycellStatement node)
-    {
+    public void inAArraycellStatement(AArraycellStatement node) {
         String varName = Utils.extractId(node.getIdentifier()).toString();
         Type type = Utils.extractType(node.getVal(), functions, vars, currentFunc);
         vars.put(varName, type);
     }
 
     @Override
-    public void inAFunctionCall(AFunctionCall node)
-    {
+    public void inAFunctionCall(AFunctionCall node) {
         currentFunc = new FunCall(Utils.extractId(node.getIdentifier()).toString());
     }
 
-    class FunCall
-    {
+    //When we leave a numeric operation we check if it is correct
+    @Override
+    public void inAAdditionExpression(AAdditionExpression node) {
+        PExpression left = node.getLpar();
+        PExpression right = node.getRpar();
+        getOperationType(left, right);
+    }
+
+    @Override
+    public void outASubtractionExpression(ASubtractionExpression node) {
+        PExpression left = node.getLpar();
+        PExpression right = node.getRpar();
+        getOperationType(left, right);
+    }
+
+    @Override
+    public void outAMultiplicationExpression(AMultiplicationExpression node) {
+        PExpression left = node.getLpar();
+        PExpression right = node.getRpar();
+        getOperationType(left, right);
+    }
+
+    @Override
+    public void outADivisionExpression(ADivisionExpression node) {
+        PExpression left = node.getLpar();
+        PExpression right = node.getRpar();
+        getOperationType(left, right);
+    }
+
+    @Override
+    public void outAModuloExpression(AModuloExpression node) {
+        PExpression left = node.getLpar();
+        PExpression right = node.getRpar();
+        getOperationType(left, right);
+    }
+
+
+    //Track current line
+    @Override
+    public void caseTId(TId node) {
+        super.caseTId(node);
+        currentLine = node.getLine();
+    }
+
+    @Override
+    public void caseTNumber(TNumber node) {
+        super.caseTNumber(node);
+        currentLine = node.getLine();
+    }
+
+    @Override
+    public void caseTNone(TNone node) {
+        super.caseTNone(node);
+        currentLine = node.getLine();
+    }
+
+    @Override
+    public void caseTString(TString node) {
+        super.caseTString(node);
+        currentLine = node.getLine();
+    }
+
+    //Flag for when we are in a return statement
+    @Override
+    public void inAReturnStatement(AReturnStatement node) {
+        returning = true;
+    }
+
+    @Override
+    public void outAReturnStatement(AReturnStatement node) {
+        returning = false;
+    }
+
+
+    //helper methods
+
+    private void getOperationType(PExpression operandA, PExpression operandB) {
+        //If we are in a return statement ignore the check since we don't know what each variable might be
+        if (!returning) {
+            Type left, right;
+            if (vars.containsKey(operandA.toString())) {
+                left = vars.get(operandA.toString());
+            } else {
+                left = Utils.extractType(operandA, functions, vars, null);
+            }
+
+            //Get b Type
+            if (vars.containsKey(operandB.toString())) {
+                right = vars.get(operandB.toString());
+            } else {
+                right = Utils.extractType(operandB, functions, vars, null);
+            }
+
+            //check that operands are of the same type
+            if ((left == Type.NUMBER && right == Type.NUMBER) || (left == Type.STRING && right == Type.STRING)) {
+                vars.put(operandA.parent().toString(), left);
+            }
+            //Else print error and set it's type as Numeric to avoid further errors
+            else {
+                System.err.println("Line " + currentLine + ": Incompatible types: " + left.toString() + " and " + right.toString());
+                vars.put(operandA.parent().toString(), Type.NUMBER);
+            }
+        }
+    }
+
+    class FunCall {
         ArrayList<Type> args;
         String name;
 
-        FunCall(String name)
-        {
+        FunCall(String name) {
             this.name = name;
             this.args = new ArrayList<>();
         }
